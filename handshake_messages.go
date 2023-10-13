nabil-ak/utls

rebased on latest refraction-networking/utls master;

handshake_messages.go
@@ -741,7 +741,7 @@ func (m *serverHelloMsg) marshal() []byte {
 				})
 			}
 			if len(m.supportedPoints) > 0 {
-				b.AddUint16(extensionSupportedPoints)
+				b.AddUint16(ExtensionSupportedPoints)
 				b.AddUint16LengthPrefixed(func(b *cryptobyte.Builder) {
 					b.AddUint8LengthPrefixed(func(b *cryptobyte.Builder) {
 						b.AddBytes(m.supportedPoints)
@@ -874,7 +874,7 @@ func (m *serverHelloMsg) unmarshal(data []byte) bool {
 			if !extData.ReadUint16(&m.selectedIdentity) {
 				return false
 			}
-		case extensionSupportedPoints:
+		case ExtensionSupportedPoints:
 			// RFC 4492, Section 5.1.2
 			if !readUint8LengthPrefixed(&extData, &m.supportedPoints) ||
 				len(m.supportedPoints) == 0 {

sleeyax/bogdanfinn-utls

handshake_messages.go
@@ -761,7 +761,7 @@ func (m *serverHelloMsg) marshal() ([]byte, error) {
 		})
 	}
 	if len(m.supportedPoints) > 0 {
-		exts.AddUint16(extensionSupportedPoints)
+		exts.AddUint16(ExtensionSupportedPoints)
 		exts.AddUint16LengthPrefixed(func(exts *cryptobyte.Builder) {
 			exts.AddUint8LengthPrefixed(func(exts *cryptobyte.Builder) {
 				exts.AddBytes(m.supportedPoints)
@@ -912,7 +912,7 @@ func (m *serverHelloMsg) unmarshal(data []byte) bool {
 			if !extData.ReadUint16(&m.selectedIdentity) {
 				return false
 			}
-		case extensionSupportedPoints:
+		case ExtensionSupportedPoints:
 			// RFC 4492, Section 5.1.2
 			if !readUint8LengthPrefixed(&extData, &m.supportedPoints) ||
 				len(m.supportedPoints) == 0 {